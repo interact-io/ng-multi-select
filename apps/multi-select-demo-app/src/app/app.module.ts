dd5c876d6d68482f4dc855c5b19df048be86c527
Paul <paul.vanuytvinck@interact.io>
2020-06-16 17:10:09 +0200

    Removed Router module

--- apps/multi-select-demo-app/src/app/app.module.ts
+++ apps/multi-select-demo-app/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
