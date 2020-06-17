8ef1c83d5a431369e23371365781611af80ad756
Paul <paul.vanuytvinck@interact.io>
2020-06-17 13:51:23 +0200

    feature/libraryComponent

---
+++ libs/ng-multi-select-lib/src/lib/components/multi-select/multi-select.component.ts
@@ -0,0 +1,71 @@
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, Optional, ViewChild } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NgControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import _ from 'lodash';
+
+@Component({
+  selector: 'interact-multi-select',
+  templateUrl: './multi-select.component.html',
+  styleUrls: ['./multi-select.component.scss']
+})
+export class MultiSelectComponent implements OnInit, ControlValueAccessor, AfterViewInit, OnDestroy {
+  @Input() fields: any[];
+  localFormGroup: FormGroup;
+  multiSelectChangeSubscription: Subscription;
+  showResults: boolean;
+  @ViewChild('multiSelectListContainer') multiSelectListContainer: ElementRef;
+  onChange: (value: any[]) => {};
+  onTouched: () => {};
+
+  constructor(@Optional() public ngControl: NgControl, private formBuilder: FormBuilder) {
+    if (ngControl) {
+      ngControl.valueAccessor = this;
+    }
+  }
+
+  ngOnInit(): void {
+    this.localFormGroup = this.formBuilder.group({
+      values: this.ngControl ? [this.ngControl.value || [], this.ngControl.control.validator, this.ngControl.control.asyncValidator] : []
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.multiSelectChangeSubscription = this.localFormGroup.get('values').valueChanges.subscribe((val: any[]) => {
+      this.updateInput(val);
+    });
+  }
+
+  updateInput(val: string[]) {
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  writeValue(value: any[]): void {
+    if (value && this.localFormGroup) {
+      this.localFormGroup.get('values').setValue(value);
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  ngOnDestroy(): void {
+    if (this.multiSelectChangeSubscription) {
+      this.multiSelectChangeSubscription.unsubscribe();
+    }
+  }
+
+  selectOption(field: any) {
+    this.localFormGroup.controls['values'].patchValue(_.xor(this.localFormGroup.controls['values'].value, [field]));
+  }
+
+  toggleMultiSelect() {
+    this.multiSelectListContainer.nativeElement.focus();
+  }
+
+}
--- libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
+++ libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MultiSelectComponent } from './components/multi-select/multi-select.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
+  declarations: [MultiSelectComponent],
+  exports: [
+    MultiSelectComponent
+  ]
 })
 export class NgMultiSelectLibModule {}
