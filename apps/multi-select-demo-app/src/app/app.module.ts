16c579dcdadd8964a90e08245fc8e190ac670e31
Paul <paul.vanuytvinck@interact.io>
2020-06-18 11:20:36 +0200

    feature/demoApp

--- apps/multi-select-demo-app/src/app/app.module.ts
+++ apps/multi-select-demo-app/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgMultiSelectLibModule } from '@ng-multi-select/ng-multi-select-lib';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MultiSelectFormComponent } from './components/multi-select-form/multi-select-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MultiSelectFormComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgMultiSelectLibModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
