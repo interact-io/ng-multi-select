223a8c440ddf14d56543cdd39be1e2b37876341a
Paul <paul.vanuytvinck@interact.io>
2020-06-18 17:02:34 +0200

    Added an option component

--- libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
+++ libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MultiSelectComponent } from './components/multi-select/multi-select.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToOptionPipe } from './pipes/to-option.pipe';
+import { MultiSelectOptionComponent } from './components/multi-select-option/multi-select-option.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [MultiSelectComponent, ToOptionPipe],
+  declarations: [MultiSelectComponent, ToOptionPipe, MultiSelectOptionComponent],
   exports: [
-    MultiSelectComponent
+    MultiSelectComponent,
+    MultiSelectOptionComponent
   ],
   providers: [ToOptionPipe]
 })
