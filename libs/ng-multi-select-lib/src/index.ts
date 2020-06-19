dc1bb577fd47cea2101ba2a1ef6d217867d0190d
Paul <paul.vanuytvinck@interact.io>
2020-06-19 15:03:11 +0200

    Updated index ts for lib and consistency update for github actions

--- libs/ng-multi-select-lib/src/index.ts
+++ libs/ng-multi-select-lib/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/ng-multi-select-lib.module';
+
+export * from './lib/components/multi-select/multi-select.component';
+export * from './lib/components/multi-select-option/multi-select-option.component';
