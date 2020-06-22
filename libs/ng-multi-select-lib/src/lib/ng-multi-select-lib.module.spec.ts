4ee7188409f2d63c40c43e5da045341e6fda1552
Paul <paul.vanuytvinck@interact.io>
2020-06-22 18:38:45 +0200

    Added module import in main test file

--- libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.spec.ts
+++ libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.spec.ts
@@ -1,10 +1,11 @@
 import { async, TestBed } from '@angular/core/testing';
 import { NgMultiSelectLibModule } from '@ng-multi-select/ng-multi-select-lib';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('NgMultiSelectLibModule', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [NgMultiSelectLibModule]
+      imports: [NgMultiSelectLibModule, ReactiveFormsModule, FormsModule]
     }).compileComponents();
   }));
 
