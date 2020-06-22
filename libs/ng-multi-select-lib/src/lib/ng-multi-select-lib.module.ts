2763afc3e1fece37fa74006cad922181ec933342
Paul <paul.vanuytvinck@interact.io>
2020-06-22 17:19:40 +0200

    Added FormsModule import

--- libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
+++ libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MultiSelectComponent } from './components/multi-select/multi-select.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MultiSelectOptionComponent } from './components/multi-select-option/multi-select-option.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   declarations: [MultiSelectComponent, MultiSelectOptionComponent],
   exports: [
     MultiSelectComponent,
