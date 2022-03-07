f1dddfe62b8afed50f2fd18f1a213363928bcbbc
Duy Tuan Anh Vu <vuduytuananh@gmail.com>
2022-03-07 14:14:38 +0100

    npx @angular/cli@11 update @angular/core@11 @angular/cli@11

--- apps/multi-select-demo-app/src/app/components/multi-select-form/multi-select-form.component.spec.ts
+++ apps/multi-select-demo-app/src/app/components/multi-select-form/multi-select-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MultiSelectFormComponent } from './multi-select-form.component';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -7,7 +7,7 @@ describe('MultiSelectFormComponent', () => {
   let component: MultiSelectFormComponent;
   let fixture: ComponentFixture<MultiSelectFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [MultiSelectFormComponent],
       imports: [FormsModule, ReactiveFormsModule],
--- libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.spec.ts
+++ libs/ng-multi-select-lib/src/lib/ng-multi-select-lib.module.spec.ts
@@ -1,9 +1,9 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { NgMultiSelectLibModule } from '@ng-multi-select/ng-multi-select-lib';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('NgMultiSelectLibModule', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [NgMultiSelectLibModule, ReactiveFormsModule, FormsModule]
     }).compileComponents();
